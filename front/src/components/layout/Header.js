76f658247b248857ba588465bf18531dd2b047a4
Albertosb10 <albertosb10@outlook.com>
2022-11-13 12:08:24 -0500

    search fix

--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react'
 import { Link } from 'react-router-dom'
 import "../../App.css"
+import { Search } from './Search'
 
 const Header = () => {
 
@@ -9,23 +10,13 @@ const Header = () => {
             <nav className='navbar row'>
                 <div className='col-12 col-md-3'>
                     <div className='navbar-brand'>
-                        <Link to="/"> <img src="./images/logo.png" class = "img-fluid" alt="SBS Logo"></img></Link>
+                        <Link to="/"> <img src="../images/logo.png" class = "img-fluid" alt="SBS Logo"></img></Link>
                     </div>
                 </div>
 
                 <div className='col-12 col-md-6 mt-2 mt-md-0'>
-                    <div className="input-group">
-                        <input
-                            type="text"
-                            id="search_field"
-                            class="form-control"
-                            placeholder='Que producto busca?'></input>
-                        <div class="input-group-append">
-                            <button id="search-btn" class="btn">
-                                <i class="fa fa-search-plus fa-3x text-white" aria-hidden="true"></i>
-                            </button>
-                        </div>
-                    </div>
+                {/*Aqui va buscar*/}
+                <Search />
                 </div>
                 <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
                 <div className="ml-4 dropdown d-inline">

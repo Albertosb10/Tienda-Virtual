11c853aa578587eb96aaa8f1cc7966ace6891521
Albertosb10 <albertosb10@outlook.com>
2022-11-16 12:48:31 -0500

    cloudinay configuration

--- Back/server.js
+++ Back/server.js
@@ -1,5 +1,7 @@
 const app = require("./app")
 const connectDatabase = require("./config/database")
+const cloudinary= require("cloudinary")
+
 
 //setting configuration file 
 const dotenv = require("dotenv");
@@ -7,6 +9,13 @@ dotenv.config({path: 'Back/config/config.env'})
 
 //database configuration
 connectDatabase();
+
+//Configurar Cloudinary
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 //server
 const server = app.listen(process.env.PORT, () => {
